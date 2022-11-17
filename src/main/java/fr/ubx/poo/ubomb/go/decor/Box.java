95a4499d9a5c972c62b5b3ac9a9a2780ebabba7d
Thibault PERRIER <thibault.perrier.1@etu.u-bordeaux.fr>
2022-11-17 15:37:31 +0100

    canMove player + box(TODO)

--- src/main/java/fr/ubx/poo/ubomb/go/decor/Box.java
+++ src/main/java/fr/ubx/poo/ubomb/go/decor/Box.java
@@ -1,9 +1,43 @@
 package fr.ubx.poo.ubomb.go.decor;
 
+import fr.ubx.poo.ubomb.game.Direction;
 import fr.ubx.poo.ubomb.game.Position;
+import fr.ubx.poo.ubomb.go.Movable;
 
-public class Box extends Decor {
+public class Box extends Decor implements Movable {
 	public Box(Position position) {
 		super(position);
+	}
+
+	public boolean canMove(Direction direction){
+		boolean canMove;
+		int dx = 0, dy = 0;
+		switch (direction) {
+			case UP ->{
+				dy--;
+				//canMove = game.getGameObjects(new Position()) != 0;
+			}
+			case DOWN ->{
+				dy++;
+				canMove = game.player().getPosition().y() != game.grid().height() - 1;
+			}
+			case LEFT ->{
+				dx--;
+				canMove = game.player().getPosition().x() != 0;
+			}
+			case RIGHT ->{
+				dx++;
+				canMove = game.player().getPosition().x() != game.grid().width() - 1;
+			}
+			default ->
+					throw new RuntimeException("direction in the 3rd dimension");
+		}
+		return true;
+		//return canMove;
+		//if (new Position(this.getPosition().x() + dx, this.getPosition().y() + dy).get )
+	}
+
+	public void doMove(Direction direction){
+		return;
 	}
 }

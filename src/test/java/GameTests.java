67579c2c96c8f7593eb0fdfb4f4fec7cc48a11cb
TO <theophyl.hoguet@etu.u-bordeaux.fr>
2022-11-25 06:53:53 +0100

    finsihed

--- src/test/java/GameTests.java
+++ src/test/java/GameTests.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import fr.ubx.poo.ubomb.game.Configuration;
